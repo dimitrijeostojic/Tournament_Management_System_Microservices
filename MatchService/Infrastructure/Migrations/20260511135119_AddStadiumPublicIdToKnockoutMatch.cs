9f0f8573751db4dea7c02cecf828df8a4b96acbd
Dimitrije Ostojic <dimitrijeo@invaryant.com>
2026-05-11 15:54:26 +0200

    add scheduledAt and StadiumPublicId in KnockoutMatch

---
+++ MatchService/Infrastructure/Migrations/20260511135119_AddStadiumPublicIdToKnockoutMatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStadiumPublicIdToKnockoutMatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledAt",
+                table: "KnockoutMatches",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StadiumPublicId",
+                table: "KnockoutMatches",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScheduledAt",
+                table: "KnockoutMatches");
+
+            migrationBuilder.DropColumn(
+                name: "StadiumPublicId",
+                table: "KnockoutMatches");
+        }
+    }
+}
